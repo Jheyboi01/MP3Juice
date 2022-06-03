f6bd27c9798d8ca0f929c4fc8029912e92e41d02
arbiodunabey@gmail.com <arbiodunabey@gmail.com>
2022-06-03 02:08:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/**var p = document.querySelector('p');
+
+p.style.border = '3px dotted blue';
+p.style.textAlign = 'right';
+
+p.onClick = function(){
+  this.style.color = 'white';
+  this.style.backgroundColor = 'blue';  
+}
+**/
